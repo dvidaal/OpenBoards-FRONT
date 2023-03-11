0feca28a197554b74a3ead225e390d4fad0c8358
Diana Vidal <dvidaal@gmail.com>
2023-03-11 19:35:57 +0100

    Create function to render router routes

---
+++ src/testUtils/renderWithProviders.tsx
@@ -0,0 +1,44 @@
+import { RouterProvider } from "react-router-dom";
+import { PreloadedState } from "@reduxjs/toolkit";
+import { render } from "@testing-library/react";
+import { PropsWithChildren } from "react";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
+import { getComponentRouter, router } from "../routers/routes";
+import { RootState, setupStore, store } from "../store";
+import GlobalStyles from "../styles/GlobalStyles";
+import theme from "../styles/theme";
+
+const renderWithProviders = (
+  ui: React.ReactElement,
+  preloadedState?: PreloadedState<RootState>
+) => {
+  const testStore = preloadedState ? setupStore(preloadedState) : store;
+
+  const Wrapper = ({ children }: PropsWithChildren): JSX.Element => {
+    return (
+      <Provider store={testStore}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          {children}
+        </ThemeProvider>
+      </Provider>
+    );
+  };
+
+  return render(ui, { wrapper: Wrapper });
+};
+
+export default renderWithProviders;
+
+export const renderRouterWithProviders = (
+  ui?: React.ReactElement,
+  preloadedState?: PreloadedState<RootState>
+) => {
+  const routerWithProvider = ui ? getComponentRouter(ui) : router;
+
+  return renderWithProviders(
+    <RouterProvider router={routerWithProvider}></RouterProvider>,
+    preloadedState
+  );
+};
