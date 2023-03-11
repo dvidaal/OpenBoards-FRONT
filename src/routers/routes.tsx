04730e199ad33f1b86f954734bb10954ea36752e
Diana Vidal <dvidaal@gmail.com>
2023-03-11 19:35:11 +0100

    Create router structure and routes need it

---
+++ src/routers/routes.tsx
@@ -0,0 +1,26 @@
+import { RouteObject, createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import { endpoints } from "./types";
+
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <ProtectedRoute element={<App />} /> },
+      { path: endpoints.login, element: <LoginPage /> },
+    ],
+  },
+];
+
+export const router = createBrowserRouter(routes);
+
+export const getComponentRouter = (ui: React.ReactElement) =>
+  createBrowserRouter([
+    {
+      path: "/",
+      element: ui,
+    },
+  ]);
