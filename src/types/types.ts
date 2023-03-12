a478e94dd8c9069b14eb67442fdeb39e97958a4a
Diana Vidal <dvidaal@gmail.com>
2023-03-12 18:02:00 +0100

    Create gameSlice to load games

--- src/types/types.ts
+++ src/types/types.ts
@@ -4,3 +4,14 @@ interface ModalPayload {
 }
 
 export default ModalPayload;
+
+export interface GameStructure {
+  game: string;
+  avatar: string;
+  date: Date;
+  hour: string;
+  bio: string;
+  plazasLibres: number;
+}
+
+export type GamesStructure = GameStructure[];
