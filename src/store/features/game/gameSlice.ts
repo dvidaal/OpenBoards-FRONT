6b2de6aebcde8dfd08fb816cf9345415bed6d2ee
Diana Vidal <dvidaal@gmail.com>
2023-03-15 13:47:34 +0100

    Create slice for getGameById and test

--- src/store/features/game/gameSlice.ts
+++ src/store/features/game/gameSlice.ts
@@ -1,18 +1,43 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { GamesData, GamesStructure } from "../../../types/types";
+import { GamesData, GamesStructure, GameStructure } from "../../../types/types";
 
-const initialGameState: GamesData = { games: [] };
+const initialGameState: GamesData = {
+  games: [],
+  singleGame: {
+    game: "",
+    avatar: "",
+    date: "",
+    hour: "",
+    bio: "",
+    plazasLibres: 2,
+    id: "",
+  },
+};
 
 const gameSlice = createSlice({
   name: "game",
   initialState: initialGameState,
   reducers: {
-    loadGames: (currentGameState, action: PayloadAction<GamesStructure>) => ({
+    loadGames: (
+      currentGameState,
+      action: PayloadAction<GamesStructure>
+    ): GamesData => ({
       ...currentGameState,
       games: [...action.payload],
+    }),
+
+    loadOneGame: (
+      currentGameState,
+      action: PayloadAction<GameStructure>
+    ): GamesData => ({
+      ...currentGameState,
+      singleGame: { ...action.payload },
     }),
   },
 });
 
 export const gameReducer = gameSlice.reducer;
-export const { loadGames: loadGamesActionCreator } = gameSlice.actions;
+export const {
+  loadGames: loadGamesActionCreator,
+  loadOneGame: loadOneGameActionCreator,
+} = gameSlice.actions;
