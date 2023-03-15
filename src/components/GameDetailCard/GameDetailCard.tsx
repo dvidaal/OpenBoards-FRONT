7da2a2076713b9dfb7841cf7a4111f124f46078f
Diana Vidal <dvidaal@gmail.com>
2023-03-15 20:57:05 +0100

    Create detail card with test and styles

---
+++ src/components/GameDetailCard/GameDetailCard.tsx
@@ -0,0 +1,29 @@
+import { GameStructure } from "../../types/types";
+import GameDetailCardStyled from "./GameDetailCardStyled";
+
+interface CardDetailProps {
+  game: GameStructure;
+}
+
+const GameDetailCard = ({ game }: CardDetailProps): JSX.Element => {
+  return (
+    <GameDetailCardStyled>
+      <img
+        src={game.avatar}
+        alt="Cover del juego"
+        className="detail-image"
+        width="250"
+        height="250"
+      />
+      <h2 className="detail-title">Partida de {game.game}</h2>
+      <span className="detail-date">{game.date}</span>
+      <span className="detail-freespace">
+        {game.plazasLibres} plazas libres
+      </span>
+      <span className="detail-bio">Sobre la partida</span>
+      <span className="detail-bio-about">{game.bio}</span>
+    </GameDetailCardStyled>
+  );
+};
+
+export default GameDetailCard;
