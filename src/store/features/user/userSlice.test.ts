058fe9636741a3c08309dbf1ee05b818effff880
Diana Vidal <dvidaal@gmail.com>
2023-03-18 22:57:07 +0100

    Create slice to logout

--- src/store/features/user/userSlice.test.ts
+++ src/store/features/user/userSlice.test.ts
@@ -1,5 +1,10 @@
 import { UserState } from "./types";
-import { initialState, loginUserActionCreator, userReducer } from "./userSlice";
+import {
+  initialState,
+  loginUserActionCreator,
+  logoutUserActionCreator,
+  userReducer,
+} from "./userSlice";
 
 describe("Given userReducer reducer", () => {
   describe("When it receives a user with an action to log in", () => {
@@ -22,6 +27,16 @@ describe("Given userReducer reducer", () => {
       const expectedUserState = userReducer(initialState, loginUser);
 
       expect(expectedUserState).toStrictEqual(loggedUserStatus);
+    });
+  });
+
+  describe("When it receives a new state and the action to log out the user", () => {
+    test("Then it should return the user with isLogged property set to false", () => {
+      const logourUserAction = logoutUserActionCreator();
+
+      const newUserState = userReducer(initialState, logourUserAction);
+
+      expect(newUserState).toStrictEqual(initialState);
     });
   });
 });
