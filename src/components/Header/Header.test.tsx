9f383e4a7841cf32352d2f25ffdc4002b75d0fc6
Diana Vidal <dvidaal@gmail.com>
2023-03-18 22:57:38 +0100

    Add logout in navigation bar and test it

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,5 +1,5 @@
 import { screen } from "@testing-library/react";
-import { renderWithProviders } from "../../testUtils/renderWithProviders";
+import { renderRouterWithProviders } from "../../testUtils/renderWithProviders";
 import Header from "./Header";
 
 describe("Given a Header component", () => {
@@ -7,9 +7,12 @@ describe("Given a Header component", () => {
     test("Then it should show a logo if the user is logged", () => {
       const expectText = "Logo OpenBoards";
 
-      renderWithProviders(<Header />, {
-        user: { isLogged: true, token: "", username: "", id: "" },
-      });
+      renderRouterWithProviders(
+        {
+          user: { isLogged: true, token: "", username: "", id: "" },
+        },
+        <Header />
+      );
 
       const result = screen.getByAltText(expectText);
 
--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,24 +1,33 @@
 import { NavLink } from "react-router-dom";
+import useUser from "../../hooks/useUser/useUser";
+import { useAppSelector } from "../../store/hooks";
 import NavigationBarStyled from "./NavigationBarStyled";
 
 const NavigationBar = (): JSX.Element => {
+  const { isLogged } = useAppSelector((state) => state.user);
+  const { logoutUser } = useUser();
   return (
     <NavigationBarStyled>
-      <NavLink to="/">
-        <img
-          src="/house.webp"
-          alt="Icono de una casa para ir al inicio de la app"
-          width="36"
-          height="36"
-        />
-      </NavLink>
-      <img
-        src="/logout.webp"
-        alt="Icono para cerrar sesión"
-        width="36"
-        height="36"
-        className="logout"
-      />
+      {isLogged && (
+        <>
+          <NavLink to="/">
+            <img
+              src="/house.webp"
+              alt="Icono de una casa para ir al inicio de la app"
+              width="36"
+              height="36"
+            />
+          </NavLink>
+          <img
+            src="/logout.webp"
+            alt="Icono para cerrar sesión"
+            width="36"
+            height="36"
+            className="logout"
+            onClick={logoutUser}
+          />
+        </>
+      )}
     </NavigationBarStyled>
   );
 };
