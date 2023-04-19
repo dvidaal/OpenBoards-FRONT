3dba12fc855b8414d99454055ff04732356ed16b
Diana Vidal <dvidaal@gmail.com>
2023-04-19 12:21:35 +0200

    Create custom hook for user register

--- src/hooks/useUser/useUser.ts
+++ src/hooks/useUser/useUser.ts
@@ -1,7 +1,12 @@
 import decodeToken from "jwt-decode";
 import { useAppDispatch } from "../../store/hooks";
 import { User } from "../../store/features/user/types";
-import { CustomTokenPayload, LoginResponse, UserCredentials } from "./types";
+import {
+  CustomTokenPayload,
+  LoginResponse,
+  RegisterCredentials,
+  UserCredentials,
+} from "./types";
 import {
   loginUserActionCreator,
   logoutUserActionCreator,
@@ -17,6 +22,7 @@ import { useNavigate } from "react-router-dom";
 export interface UserStructure {
   loginUser: (userCredentials: UserCredentials) => Promise<void>;
   logoutUser: () => void;
+  registerUser: (registerCredentials: RegisterCredentials) => Promise<void>;
 }
 
 const useUser = (): UserStructure => {
@@ -27,6 +33,7 @@ const useUser = (): UserStructure => {
   const apirUrl = process.env.REACT_APP_URL_API;
   const appEndpoint = "/users";
   const loginEndpoint = "/login";
+  const registerEndpoint = "/register";
 
   const loginUser = async (userCredentials: UserCredentials) => {
     try {
@@ -68,7 +75,46 @@ const useUser = (): UserStructure => {
     dispatch(logoutUserActionCreator());
   };
 
-  return { loginUser, logoutUser };
+  const registerUser = async (registerCredentials: RegisterCredentials) => {
+    try {
+      dispatch(setLoaderActionCreator());
+
+      const response = await fetch(
+        `${apirUrl}${appEndpoint}${registerEndpoint}`,
+        {
+          method: "POST",
+          body: JSON.stringify(registerCredentials),
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+
+      dispatch(unsetLoaderActionCreator());
+
+      if (!response.ok) {
+        throw new Error("No ha sido posible registrarse. Reivsa los campos.");
+      }
+
+      dispatch(
+        showModalActionCreator({
+          modal: "Usuario correctamente creado.",
+          isError: false,
+          isLoading: false,
+          isSucces: true,
+        })
+      );
+    } catch {
+      dispatch(
+        showModalActionCreator({
+          modal: "No se ha podido crear el usuario.",
+          isError: true,
+          isLoading: false,
+          isSucces: false,
+        })
+      );
+    }
+  };
+
+  return { loginUser, logoutUser, registerUser };
 };
 
 export default useUser;
